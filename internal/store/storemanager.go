ebb1a2a991b9a2edf0ebccc3a8aa1f99a48baf71
w.richter@telekom.de <w.richter@telekom.de>
2025-09-16 01:41:40 +0200

    feat: initial refactored store implementation

---
+++ internal/store/storemanager.go
@@ -0,0 +1,213 @@
+package store
+
+import (
+	"github.com/rs/zerolog/log"
+	"github.com/telekom/quasar/internal/config"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/dynamic"
+	"sync"
+)
+
+type DualStore interface {
+	Store
+	GetPrimary() Store
+	GetSecondary() Store
+}
+
+// DualStoreManager manages multiple stores (primary and secondary)
+type DualStoreManager struct {
+	primary      Store
+	secondary    Store
+	mu           sync.RWMutex
+	errorHandler ErrorHandler
+}
+
+// ErrorHandler handles store operation errors
+type ErrorHandler interface {
+	HandlePrimaryError(operation string, err error) error
+	HandleSecondaryError(operation string, err error)
+}
+
+// DefaultErrorHandler provides default error handling
+type DefaultErrorHandler struct{}
+
+func (h *DefaultErrorHandler) HandlePrimaryError(operation string, err error) error {
+	return err
+}
+
+func (h *DefaultErrorHandler) HandleSecondaryError(operation string, err error) {
+	// Log secondary errors but don't fail the operation
+	log.Warn().Err(err).Str("operation", operation).Msg("Secondary store operation failed")
+}
+
+func SetupDualStoreManager(primaryType, secondaryType string) (DualStore, error) {
+	if primaryType == "" {
+		return nil, ErrUnknownStoreType
+	}
+
+	// Create primary store
+	primary, err := createStore(primaryType)
+	if err != nil {
+		log.Fatal().Fields(map[string]any{
+			"primaryType":   primaryType,
+			"secondaryType": secondaryType,
+		}).Err(err).Msg("Could not create store manager!")
+		return nil, err
+	}
+
+	// Create secondary store
+	var secondary Store
+	if secondaryType != "" && secondaryType != primaryType {
+		secondary, err = createStore(secondaryType)
+		if err != nil {
+			log.Fatal().Fields(map[string]any{
+				"primaryType":   primaryType,
+				"secondaryType": secondaryType,
+			}).Err(err).Msg("Could not create store manager!")
+			return nil, err
+		}
+	}
+
+	// Create and return the DualStoreManager
+	manager := &DualStoreManager{
+		primary:      primary,
+		secondary:    secondary,
+		mu:           sync.RWMutex{},
+		errorHandler: &DefaultErrorHandler{},
+	}
+
+	manager.Initialize()
+	return manager, nil
+}
+
+func (m *DualStoreManager) Initialize() {
+
+	if m.primary != nil {
+		m.primary.Initialize()
+	}
+	if m.secondary != nil {
+		m.secondary.Initialize()
+	}
+}
+
+func (m *DualStoreManager) InitializeResource(kubernetesClient dynamic.Interface, resourceConfig *config.ResourceConfiguration) {
+
+	if m.primary != nil {
+		m.primary.InitializeResource(kubernetesClient, resourceConfig)
+	}
+	if m.secondary != nil {
+		m.secondary.InitializeResource(kubernetesClient, resourceConfig)
+	}
+}
+
+func (m *DualStoreManager) OnAdd(obj *unstructured.Unstructured) error {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	var primaryErr error
+
+	if m.primary != nil {
+		if primaryErr = m.primary.OnAdd(obj); primaryErr != nil {
+			primaryErr = m.errorHandler.HandlePrimaryError("OnAdd", primaryErr)
+		}
+	}
+	if m.secondary != nil {
+		go func() {
+			if secondaryErr := m.secondary.OnAdd(obj); secondaryErr != nil {
+				m.errorHandler.HandleSecondaryError("OnAdd", secondaryErr)
+			}
+		}()
+	}
+
+	return primaryErr
+}
+
+func (m *DualStoreManager) OnUpdate(oldObj *unstructured.Unstructured, newObj *unstructured.Unstructured) error {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	var primaryErr error
+
+	if m.primary != nil {
+		if primaryErr = m.primary.OnUpdate(oldObj, newObj); primaryErr != nil {
+			primaryErr = m.errorHandler.HandlePrimaryError("OnUpdate", primaryErr)
+		}
+	}
+
+	if m.secondary != nil {
+		go func() {
+			if secondaryErr := m.secondary.OnUpdate(oldObj, newObj); secondaryErr != nil {
+				m.errorHandler.HandleSecondaryError("OnUpdate", secondaryErr)
+			}
+		}()
+	}
+	return primaryErr
+}
+
+func (m *DualStoreManager) OnDelete(obj *unstructured.Unstructured) error {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	var primaryErr error
+
+	if m.primary != nil {
+		primaryErr = m.errorHandler.HandlePrimaryError("OnDelete", primaryErr)
+		primaryErr = primaryErr
+	}
+
+	if m.secondary != nil {
+		go func() {
+			if secondaryErr := m.secondary.OnDelete(obj); secondaryErr != nil {
+				m.errorHandler.HandleSecondaryError("OnDelete", secondaryErr)
+			}
+		}()
+	}
+
+	return primaryErr
+}
+
+func (m *DualStoreManager) Count(mapName string) (int, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	if m.primary != nil && m.primary.Connected() {
+		return m.primary.Count(mapName)
+	}
+
+	return 0, ErrNoConnectedStore
+}
+
+func (m *DualStoreManager) Keys(mapName string) ([]string, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	if m.primary != nil && m.primary.Connected() {
+		return m.primary.Keys(mapName)
+	}
+
+	return nil, ErrNoConnectedStore
+}
+
+func (m *DualStoreManager) Shutdown() {
+	if m.primary != nil {
+		m.primary.Shutdown()
+	}
+	if m.secondary != nil {
+		m.secondary.Shutdown()
+	}
+}
+
+func (m *DualStoreManager) Connected() bool {
+	if m.primary != nil && m.primary.Connected() {
+		return true
+	}
+	return false
+}
+
+func (m *DualStoreManager) GetPrimary() Store {
+	return m.primary
+}
+
+func (m *DualStoreManager) GetSecondary() Store {
+	return m.secondary
+}
